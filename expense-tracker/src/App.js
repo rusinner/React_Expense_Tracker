268253eb1babd8c3a0527970ecf9d9f7b74cca58
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-19 16:32:29 +0200

    Form Component

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import { Grid } from "@material-ui/core";
+
+import Details from "./components/Details/Details";
+import Main from "./components/Details/Main/Main";
+import useStyles from "./styles";
 
 function App() {
-  return <div className="App">App</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justifyContent="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
---
+++ expense-tracker/src/components/Details/Main/Main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance 100 €
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          INFOCOMPONENT
+        </Typography>
+        <Divider />
+        FORMCOMPONENT
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            LISTCOMPONENT
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
