27777bee8ba0de471327014dce678d636efccfc7
Theologos Mavropoulos <rusinner88@gmail.com>
2022-12-24 12:35:55 +0200

    change components order in mobile

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -21,13 +21,16 @@ function App() {
         justifyContent="center"
         style={{ height: "100vh" }}
       >
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={4} className={classes.main}>
           <Main />
         </Grid>
-        <Grid item xs={12} sm={3}>
+        <Grid item xs={12} sm={3} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.last}>
           <Details title="Expense" />
         </Grid>
       </Grid>
